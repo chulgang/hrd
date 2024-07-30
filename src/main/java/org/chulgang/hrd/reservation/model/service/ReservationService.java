50b43b0120b272f672a9431627c73e550a27afc2
HS-hyeokjin <youngurwls@gmail.com>
2024-07-30 19:57:06 +0900

    feat : 예약 리스트 요청 Service 추가

--- src/main/java/org/chulgang/hrd/reservation/model/service/ReservationService.java
+++ src/main/java/org/chulgang/hrd/reservation/model/service/ReservationService.java
@@ -1,7 +1,13 @@
 package org.chulgang.hrd.reservation.model.service;
 
+import org.chulgang.hrd.reservation.dto.ReservationCardResponse;
+
+import java.util.List;
+
 public interface ReservationService {
 
     boolean registerReservation(Long memberId, Long courseId);
+
+    List<ReservationCardResponse> getPagedReservations(Long id , int pageNumber);
 
 }
