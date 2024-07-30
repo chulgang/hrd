d006b37d172f054823951874b0694a10fc9f1ebb
HS-hyeokjin <youngurwls@gmail.com>
2024-07-31 02:54:07 +0900

    feat : 리스트 요소 PaymentCard Dto 구현

---
+++ src/main/java/org/chulgang/hrd/payment/dto/PaymentCardResponse.java
@@ -0,0 +1,60 @@
+package org.chulgang.hrd.payment.dto;
+
+import java.time.LocalDate;
+
+public class PaymentCardResponse {
+    private String courseName;
+    private String courseDescription;
+    private int payedAmount;
+    private boolean isRefunded;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public int getPayedAmount() {
+        return payedAmount;
+    }
+
+    public void setPayedAmount(int payedAmount) {
+        this.payedAmount = payedAmount;
+    }
+
+    public boolean isRefunded() {
+        return isRefunded;
+    }
+
+    public void setRefunded(boolean refunded) {
+        isRefunded = refunded;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
