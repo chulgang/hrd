ebf2713e5e39cde4980b87ad1875b89b224c7a18
hellmir <hellmir610@gmail.com>
2024-07-30 14:44:10 +0900

    feat(exception): 예외 메시지 상수 추가 (#32)

--- src/main/java/org/chulgang/hrd/exception/ExceptionMessage.java
+++ src/main/java/org/chulgang/hrd/exception/ExceptionMessage.java
@@ -1,6 +1,8 @@
 package org.chulgang.hrd.exception;
 
 public class ExceptionMessage {
+    public static final String CLOSING_RESULTSET_FAILED_EXCEPTION_MESSAGE = "ResultSet을 닫는 데 실패했습니다.";
+    public static final String CLOSING_STATEMENT_FAILED_EXCEPTION_MESSAGE = "Statement를 닫는 데 실패했습니다.";
     public static final String DATE_TIME_PARSE_EXCEPTION_MESSAGE
             = "날짜 형식만 날짜 타입으로 변환할 수 있습니다. 현재 변환 대상 값: %s";
 
@@ -15,4 +17,7 @@ public class ExceptionMessage {
 
     public static final String BLOB_PARSE_EXCEPTION_MESSAGE
             = "파일 변환 중에 예외가 발생했습니다.";
+
+    public static final String ARGUMENT_NO_VALUE_EXCEPTION_MESSAGE
+            = "입력값이 전송되지 않았습니다. 다시 시도해 주세요.";
 }
