c05748fdff6d7c216d34dc4d2a54f34b6bcddea3
kde0707 <gammaeom1@gmail.com>
2024-07-30 18:38:38 +0900

    fix: typo 삭제

--- src/main/java/org/chulgang/hrd/post/controller/PostController.java
+++ src/main/java/org/chulgang/hrd/post/controller/PostController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 @WebServlet("/elearn/post.do")
 
 public class PostController extends HttpServlet {
-    private static final long serialVersionUID = 1L;ddd
+    private static final long serialVersionUID = 1L;
 
     public void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
