fbb31512685f07f8af146cfbce4a47ee4caa9e9d
hellmir <hellmir610@gmail.com>
2024-08-01 18:29:33 +0900

    feat(time-period): 강좌 등록 시 사용 강의실의 해당 날짜와 시간대 연동 (#124)
    - 강좌 등록 시 강의실의 강의 기간과 시간대를 사용 중으로 변경
    - 강좌 등록 양식에서 개강일, 종강일과 강의실을 선택 시 시간대 선택 콤보박스에서 사용 가능한 시간대 목록만 선택 가능하게 하는 기능 추가

--- src/main/java/org/chulgang/hrd/classroom/model/service/TimePeriodService.java
+++ src/main/java/org/chulgang/hrd/classroom/model/service/TimePeriodService.java
@@ -2,9 +2,14 @@ package org.chulgang.hrd.classroom.model.service;
 
 import org.chulgang.hrd.classroom.dto.GetTimePeriodResponse;
 import org.chulgang.hrd.classroom.dto.GetTimePeriodsResponse;
+import org.chulgang.hrd.classroom.dto.UpdateUsedTimePeriodRequest;
+
+import java.time.LocalDate;
 
 public interface TimePeriodService {
-    GetTimePeriodsResponse getTimePeriodsByClassroomId(Long classroomId);
+    GetTimePeriodsResponse getUsableTimePeriodsByClassroomId(Long classroomId, LocalDate startDate, LocalDate lastDate);
 
     GetTimePeriodResponse getTimePeriod(Long id);
+
+    void updateUsedTimePeriod(UpdateUsedTimePeriodRequest updateUsedTimePeriodRequest);
 }
--- src/main/java/org/chulgang/hrd/classroom/model/service/TimePeriodServiceImpl.java
+++ src/main/java/org/chulgang/hrd/classroom/model/service/TimePeriodServiceImpl.java
@@ -1,9 +1,13 @@
 package org.chulgang.hrd.classroom.model.service;
 
+import org.chulgang.hrd.classroom.domain.TimePeriod;
 import org.chulgang.hrd.classroom.dto.GetTimePeriodResponse;
 import org.chulgang.hrd.classroom.dto.GetTimePeriodsResponse;
+import org.chulgang.hrd.classroom.dto.UpdateUsedTimePeriodRequest;
 import org.chulgang.hrd.classroom.model.repository.TimePeriodRepository;
 import org.chulgang.hrd.classroom.model.repository.TimePeriodRepositoryImpl;
+
+import java.time.LocalDate;
 
 public class TimePeriodServiceImpl implements TimePeriodService {
     private static final TimePeriodService INSTANCE = new TimePeriodServiceImpl(TimePeriodRepositoryImpl.getInstance());
@@ -18,12 +22,24 @@ public class TimePeriodServiceImpl implements TimePeriodService {
     }
 
     @Override
-    public GetTimePeriodsResponse getTimePeriodsByClassroomId(Long classroomId) {
-        return GetTimePeriodsResponse.from(timePeriodRepository.findAllByClassroomId(classroomId));
+    public GetTimePeriodsResponse getUsableTimePeriodsByClassroomId(
+            Long classroomId, LocalDate startDate, LocalDate lastDate
+    ) {
+        return GetTimePeriodsResponse.from(
+                timePeriodRepository.findUsableTimesByClassroomId(classroomId, startDate, lastDate)
+        );
     }
 
     @Override
     public GetTimePeriodResponse getTimePeriod(Long id) {
         return GetTimePeriodResponse.from(timePeriodRepository.findById(id));
+    }
+
+    @Override
+    public void updateUsedTimePeriod(UpdateUsedTimePeriodRequest updateUsedTimePeriodRequest) {
+        TimePeriod timePeriod = timePeriodRepository.findById(updateUsedTimePeriodRequest.getId());
+        if (updateUsedTimePeriodRequest.updateDates(timePeriod)) {
+            timePeriodRepository.updateUsedTimePeriodById(updateUsedTimePeriodRequest);
+        }
     }
 }
