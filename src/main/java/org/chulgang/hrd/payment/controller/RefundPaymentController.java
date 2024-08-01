d206978c638b061240c4773fc7fc88ea55a0dc49
HS-hyeokjin <youngurwls@gmail.com>
2024-08-01 10:10:24 +0900

    feat : 환불 POST 컨트롤러 구현

---
+++ src/main/java/org/chulgang/hrd/payment/controller/RefundPaymentController.java
@@ -0,0 +1,45 @@
+package org.chulgang.hrd.payment.controller;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.chulgang.hrd.aop.LoggingAspect;
+import org.chulgang.hrd.payment.model.service.PaymentService;
+
+import java.io.IOException;
+
+import static org.chulgang.hrd.course.util.RequestConstant.PAYMENT_SERVICE_ATTRIBUTE_NAME;
+
+@WebServlet(urlPatterns = {"/elearn/refund-payment"})
+public class RefundPaymentController extends HttpServlet {
+    private PaymentService paymentService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        paymentService = LoggingAspect.createProxy(PaymentService.class,
+                (PaymentService) config.getServletContext().getAttribute(PAYMENT_SERVICE_ATTRIBUTE_NAME));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        //User user = (User) session.getAttribute("member");
+        //Long userId = user.getUserId();
+
+        Long userId = 1L;
+        Long courseId = Long.parseLong(request.getParameter("courseId"));
+
+        boolean isRefundSuccessful = paymentService.refundPayment(userId, courseId);
+
+        if (isRefundSuccessful) {
+            response.sendRedirect(request.getContextPath() + "/refund-success");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/404");
+        }
+    }
+}
